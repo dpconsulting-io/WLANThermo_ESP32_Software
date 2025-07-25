b1de7f4986428b36338741852f184056b97f73ba
dpconsulting-io <67409544+dpconsulting-io@users.noreply.github.com>
2025-07-25 17:14:29 +0200

    Update TemperatureSensors.h

--- src/temperature/TemperatureSensors.h
+++ src/temperature/TemperatureSensors.h
@@ -74,6 +74,6 @@ const SensorTypeInfoType sensorTypeInfo[NUM_OF_TYPES] = {
     {SensorType::TypeK, "Typ K", true},
     {SensorType::Ble, "Bluetooth", true},
     {SensorType::MaverickRadio, "Maverick", true},
-    {SensorType::SousVide, "SousVide graues Kabel", false},
-    {SensorType::Raeucherofen, "Räucherofen blaues Kabel", false},
+    //{SensorType::SousVide, "SousVide graues Kabel", false},
+    //{SensorType::Raeucherofen, "Räucherofen blaues Kabel", false},
     {SensorType::Connect, "Connect", true}};
